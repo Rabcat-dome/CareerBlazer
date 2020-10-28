c51d954f46f84cbc8e8c5ef73c0388d9abf41df6
THAIPETROLE-001\Administrator <thapp>
2020-10-28 16:50:01 +0700

    fix Date formate Client Server Issue

--- CareerBlazer/CareerBlazer.Client/Program.cs
+++ CareerBlazer/CareerBlazer.Client/Program.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,11 +13,29 @@ namespace CareerBlazer.Client
     {
         public static async Task Main(string[] args)
         {
+            CultureInfo.CurrentCulture = new CultureInfo("en-GB");
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddDevExpressBlazor();
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                // Define the list of cultures your app will support
+                var supportedCultures = new List<System.Globalization.CultureInfo>()
+                {
+                    new System.Globalization.CultureInfo("en-US"),
+                };
+
+                // Set the default culture
+                options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US");
+
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<Microsoft.AspNetCore.Localization.IRequestCultureProvider>() {
+                 new Microsoft.AspNetCore.Localization.QueryStringRequestCultureProvider()
+                };
+            });
 
             await builder.Build().RunAsync();
         }
